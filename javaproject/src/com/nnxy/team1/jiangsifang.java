a76c46ea00f2f9b4318116bc9998a7661efa7609
azongxzz <741253524@qq.com>
2021-12-07 15:22:36 +0800

    第五次实验

--- javaproject/src/com/nnxy/team1/jiangsifang.java
+++ javaproject/src/com/nnxy/team1/jiangsifang.java
@@ -3,6 +3,8 @@ package com.nnxy.team1;
 public class jiangsifang {
     public void test(){
        System.out.println("hello word!");
+       System.out.println("hello word!");
+       System.out.println("hello word!");
     }
     
 }
